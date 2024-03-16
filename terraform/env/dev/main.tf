d2ddd2e07ed6f955956eb263dd40332129b47bab
Hasan-Uz-Zaman Ashik <hasanashik100@gmail.com>
2024-03-16 16:51:35 +0600

    updated bucket S3 poridhi-briefly-curiously-rightly-greatly-infinite-lion

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "zaman-s3-bucket"
+    bucket         = "poridhi-briefly-curiously-rightly-greatly-infinite-lion"
     key            = "terraform.tfstate"
     region         = "us-east-1"
 
